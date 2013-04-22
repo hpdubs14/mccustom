966118e8a2368b1f62b72a3f3d036e46089940bd
Paul Wanless <zigloo99@gmail.com>
2013-04-22 00:58:08 -0400

    Fixed root to error

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Mccustom::Application.routes.draw do
 
+  root :to => 'pages#home'
   get "pages/home"
 
   get "pages/service"
 
   get "pages/contact"
 
-  root :to => 'pages/home'
+
 
 
   # The priority is based upon order of creation:
